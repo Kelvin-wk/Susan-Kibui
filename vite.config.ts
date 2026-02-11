9e9c14ce88daee1bfa36257936dad17c3b3ad0eb
Haiz Kelvin <haizkelvin1@gmail.com>
2026-02-11 18:25:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
-  // Set the base to your repository name for GitHub Pages
-  base: '/Susan-Market/'
+  // Must match your GitHub repo name exactly
+  base: '/Susan-Kibui/'
 });
