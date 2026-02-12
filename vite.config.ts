11500dbc5f7145c617210a53f0fe4aea97eb5850
Haiz Kelvin <haizkelvin1@gmail.com>
2026-02-12 08:00:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
+    // This allows process.env.API_KEY to be replaced during build time by Vercel's environment variables
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
-  // Must match your GitHub repo name exactly
-  base: '/Susan-Kibui/',
-})
+  // Vercel expects the app to be served from the root
+  base: '/'
+});
 
